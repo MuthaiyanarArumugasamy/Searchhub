ef44eaf6118f3101700949661169bcc658071b10
MuthaiyanarArumugasamy <Priyadharshini23407@gmail.com>
2026-07-19 15:05:53 +0530

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,13 @@
+/* =====================================
+   SearchHub AI
+   config.js
+===================================== */
+
+// Gemini API Key
+// Replace with your own API key
+
+const GEMINI_API_KEY = "PASTE_YOUR_GEMINI_API_KEY_HERE";
+
+// Gemini Model
+
+const GEMINI_MODEL = "gemini-2.5-flash";
